04f29dcd9501da3a90314ca0c445d3a5f34a5a28
IanWark <wark@ualberta.ca>
2015-11-17 09:53:22 -0700

    why1

--- Tilecoder.py
+++ Tilecoder.py
@@ -14,10 +14,10 @@ def tilecode(x,y,tileIndices):
 def printTileCoderIndices(x,y):
     tileIndices = [-1]*numTilings
     tilecode(x,y,tileIndices)
-    print 'Tile indices for input (',x,',',y,') are : ', tileIndices
+    print 'Tile indices for input (',x,',',y,') are:', tileIndices
 
-#printTileCoderIndices(0.1,0.1)
-#printTileCoderIndices(4.0,2.0)
-#printTileCoderIndices(5.99,5.99)
-#printTileCoderIndices(4.0,2.1)
+printTileCoderIndices(0.1,0.1)
+printTileCoderIndices(4.0,2.0)
+printTileCoderIndices(5.99,5.99)
+printTileCoderIndices(4.0,2.1)
     
